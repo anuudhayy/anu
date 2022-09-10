06c573fb4d545acc04b96b327185f81a744cddc7
anuudhayy <anuudhayy.94@gmail.com>
2022-09-10 12:28:28 +0530

    anu@1234

---
+++ Person.java
@@ -0,0 +1,30 @@
+package NewPro.Java;
+
+public class Person {
+	
+
+		public void main(String[] args) {
+			String name= "koushik";
+			System.out.println(name);
+			byte age=26;
+			System.out.println(age);
+			float weight=56.6f;
+			System.out.println(weight);
+			double height=155.35;
+			System.out.println(height);
+			long phonenumber=8889665366l;
+			System.out.println(phonenumber);
+			int number=152468455;
+			System.out.println(number);
+			short salary=10000;
+			System.out.println(salary);
+			char initial='k';
+			System.out.println(initial);
+			boolean like=true;
+			System.out.println(like);
+
+		}
+
+	}
+
+
